37f37c5639049149cdc3e0211095376307ce9986
Elizabeth Diaz Familia <lizzyfamilia@gmail.com>
2024-10-14 14:56:06 -0600

    Delete assets directory

--- assets/js/service/user-service.js
+++
@@ -1,6 +0,0 @@
-const listaUsuario = () => fetch("https://engaged-shiner-37.hasura.app/api/rest/consultarusuarios")
-    .then((respuesta) => respuesta.json());
-
-export const userServices = {
-    listaUsuario,
-};
