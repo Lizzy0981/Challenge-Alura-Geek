dc0e89c27b77807c77ebe015b3761cb634bd4748
Elizabeth Diaz Familia <lizzyfamilia@gmail.com>
2024-11-04 15:42:51 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const path = require('path');
-const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+const cors = require('cors');
 
 const port = process.env.PORT || 3001;
 
+server.use(cors({
+  origin: '*' // En producción, deberías especificar tu dominio de Netlify
+}));
 server.use(middlewares);
 server.use(router);
 
